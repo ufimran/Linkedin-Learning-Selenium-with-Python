917c2da1e1ae5fb95c5615222990c5ee5cbb9d3b
Umar Fauque Imran <18-36115-1@student.aiub.edu>
2023-03-29 11:41:54 +0600

    Tribel Sign In and messaging

--- TribelSignInAndMessaging.py
+++ TribelSignInAndMessaging.py
@@ -1,8 +1,5 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.support.select import Select
 import time
 
 driver = webdriver.Chrome()
@@ -17,40 +14,13 @@ driver.find_element(By.XPATH, "/html/body/div[1]/div/div/div[2]/div/div/div[3]/d
 time.sleep(1.0)
 driver.find_element(By.XPATH, "/html/body/div[1]/div/div/div[3]/div[4]/div/div/div/section/div/div/div[2]/div/div[3]/div/div[2]/div[1]/div[1]/div[1]/input").send_keys("birir19773@syswift.com")
 driver.find_element(By.XPATH, "/html/body/div[1]/div/div/div[3]/div[4]/div/div/div/section/div/div/div[2]/div/div[3]/div/div[2]/div[1]/div[1]/div[2]/input").send_keys("12345678Ii")
-time.sleep(10.0)
-# driver.find_element(By.CLASS_NAME, "rc-anchor-center-item rc-anchor-checkbox-holder").click()
+time.sleep(5)
 driver.find_element(By.XPATH, "/html/body/div[1]/div/div/div[3]/div[4]/div/div/div/section/div/div/div[2]/div/div[3]/div/div[2]/div[1]/div[2]/div/div/input").click()
 driver.implicitly_wait(10)
-driver.find_element(By.XPATH, "/html/body/div[1]/div/div/div[1]/div[2]/div[1]/div/div[3]/section/div/div/div/div[2]/div/div[2]/div/div[1]/input").send_keys("Oscar")
+driver.find_element(By.XPATH, "/html/body/div[1]/div/div/div[2]/div[3]/div[1]/div[2]/div[3]/div/div[2]/div[2]/div[1]").click()
 time.sleep(5)
+driver.find_element(By.XPATH, "/html/body/div[1]/div/div/div[2]/div[3]/div[2]/div/div/div[2]/div[2]/div[2]/textarea").send_keys("Hi")
+driver.find_element(By.XPATH, "/html/body/div[1]/div/div/div[2]/div[3]/div[2]/div/div/div[2]/div[2]/div[3]/div").click()
+time.sleep(10.0)
 
-
-
-# day = Select(driver.find_element(By.XPATH, "/html/body/div[3]/div[2]/div/div/div[2]/div/div/div[1]/form/div[1]/div[5]/div[2]/span/span/select[1]"))
-# day.select_by_visible_text("12")
-# time.sleep(1.0)
-# month = Select(driver.find_element(By.NAME, "birthday_month"))
-# month.select_by_visible_text("Jan")
-# time.sleep(1.0)
-# year = Select(driver.find_element(By.NAME, "birthday_year"))
-# year.select_by_visible_text("1999")
-#
-# time.sleep(1.0)
-# driver.find_element(By.XPATH, "//label[text()='Male']").click()
-#
-# time.sleep(1.0)
-# driver.find_element(By.XPATH, "//button[text()='Sign Up']").click()
-#
-# time.sleep(10.0)
-#
-# act_title = driver.title
-# exp_title = "Facebook-log in or sign up"
-#
-# if act_title == exp_title:
-#     print("Login Test Passed")
-# else:
-#     print("Login Test Failed")
-
-
-time.sleep(20.0)
 driver.close()
